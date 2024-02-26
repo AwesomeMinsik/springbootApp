59aba91ba88905a7eb29fed8a3334288fe402364
AwesomeMinsik <beauty3579@gmail.com>
2024-02-26 16:22:28 +0900

    24/02/26

--- src/main/java/kr/co/rland/app/controller/HomeController.java
+++ src/main/java/kr/co/rland/app/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @GetMapping("/hellooo")
+    @GetMapping("/hello")
     @ResponseBody
     public String hello(){
 
